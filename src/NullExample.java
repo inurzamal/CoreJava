462df65594c8b9f34129d8ff5fcce972fe2144a6
unknown <n.zaman98@gmail.com>
2022-07-24 21:55:19 +0530

    updated

---
+++ src/NullExample.java
@@ -0,0 +1,17 @@
+
+public class NullExample {
+	
+	void doSomething(Object o) {
+		System.out.println("Object class ");
+	}
+	
+	void doSomething(String s) {
+		System.out.println("String class invoked");
+	}
+
+	public static void main(String[] args) {
+
+		NullExample t1 = new NullExample();
+		t1.doSomething(null);
+	}
+}
