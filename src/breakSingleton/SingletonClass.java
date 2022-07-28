54d3824add32cb4fd9bf509907ca7d2a77f95e0f
Nurzamal Islam <n.zaman98@gmail.com>
2022-07-28 18:50:41 +0530

    updated

--- src/breakSingleton/SingletonClass.java
+++ src/breakSingleton/SingletonClass.java
@@ -1,22 +1,40 @@
 package breakSingleton;
 
-class SingletonClass extends MyClone{
-	
+import java.io.Serializable;
+
+class SingletonClass extends MyClone implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	private static SingletonClass x = null;
+
+	//preventing from Reflection API
+	private SingletonClass() throws IllegalAccessException {
+		if (x != null)
+			throw new IllegalAccessException("Object can't be created using reflection");
+	}
 	
-	private SingletonClass() {}
-	
+	//preventing from clone() method
+
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
 		throw new CloneNotSupportedException();
 	}
-	
-	public static SingletonClass getInstance() {
-		
-		if(x == null) {
+
+	//preventing from serialization & deserialization
+	protected Object readResolve() {
+		return x;
+	}
+
+	public static SingletonClass getInstance() throws IllegalAccessException {
+
+		if (x == null) {
 			x = new SingletonClass();
 			return x;
 		}
-		return x;	
+		return x;
 	}
 }
--- src/breakSingleton/SingletonDemo.java
+++ src/breakSingleton/SingletonDemo.java
@@ -1,13 +1,53 @@
 package breakSingleton;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.InvocationTargetException;
+
 public class SingletonDemo {
 
-	public static void main(String[] args) throws CloneNotSupportedException {
+	public static void main(String[] args) throws CloneNotSupportedException, InstantiationException, IllegalAccessException, IllegalArgumentException, 
+	InvocationTargetException, FileNotFoundException, IOException, ClassNotFoundException {
 		
 		SingletonClass instance1 = SingletonClass.getInstance();
 		System.out.println(instance1.hashCode());
 		
-		SingletonClass instance2 = (SingletonClass) instance1.clone();
+		//serialize singleton object to a file
+		ObjectOutput out = new ObjectOutputStream(new FileOutputStream("singleton.ser"));
+		out.writeObject(instance1);
+		out.close();
+		
+		// deserialize singleton object from the file
+		ObjectInput in = new ObjectInputStream(new FileInputStream("singleton.ser"));
+		SingletonClass instance2 = (SingletonClass) in.readObject();
+		in.close();
+		
 		System.out.println(instance2.hashCode());
+		
+		
+		/*
+		 * // break Singleton by clone() method SingletonClass instance2 =
+		 * (SingletonClass) instance1.clone(); System.out.println(instance2.hashCode());
+		 */
+
+		
+		/*
+		 * //break Singleton by Reflection API SingletonClass reflectionInstance = null;
+		 * 
+		 * Constructor<?>[] constructors =
+		 * SingletonClass.class.getDeclaredConstructors(); for(Constructor<?> c :
+		 * constructors) { c.setAccessible(true); reflectionInstance = (SingletonClass)
+		 * c.newInstance(); }
+		 * 
+		 * System.out.println(reflectionInstance.hashCode());
+		 */
+
+
 	}
 }
