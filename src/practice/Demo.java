f722d9225f18d50f584e79a8f94d5d7e31cecd77
unknown <n.zaman98@gmail.com>
2022-09-01 00:03:43 +0530

    updated

--- src/practice/Demo.java
+++ src/practice/Demo.java
@@ -1,10 +1,21 @@
 package practice;
 
-public class Demo {
-	
-	public static void main(String[] args) {
-		
-		System.out.println(5+6+"hello");
-		System.out.println("hello"+5+6);
+class Demo extends Thread{
+	   volatile boolean isRunning = true;
+	   public void run() {
+	      long count=0;
+	      while (isRunning) {
+	         count++;
+	      }
+	      System.out.println("Thread terminated: " + count);
+	   }
+	   public static void main(String[] args) throws InterruptedException {
+	      Demo t = new Demo();
+	      t.start();
+	      Thread.sleep(2000);
+	      t.isRunning = false;
+	      t.join();
+	      System.out.println("isRunning set to " + t.isRunning);
+	   }
 	}
-}
+
