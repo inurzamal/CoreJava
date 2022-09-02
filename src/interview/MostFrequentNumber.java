687b6cfba32ae337f2cf135d0c5212c9114e0601
Nurzamal Islam <n.zaman98@gmail.com>
2022-09-02 18:43:31 +0530

    updated

--- src/interview/MostFrequentNumber.java
+++ src/interview/MostFrequentNumber.java
@@ -1,57 +1,47 @@
 package interview;
 
 
-//Modify this code or see from another website and paste in notes also
-
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
 public class MostFrequentNumber {
 	
-	static int mostFrequent(int arr[], int n)
-    {
-         
-       
+	static int mostFrequent(int arr[])
+    {     
         Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
         
-        // Insert all elements in hash        
-        for(int i = 0; i < n; i++)
-        {
-            int key = arr[i];
-            
-            if(hm.containsKey(key))
-            {
-                hm.put(key, hm.get(key)+1);
-            }
-            else
-            {
-                hm.put(key, 1);
-            }
+        for(Integer i: arr) {
+        	if(hm.containsKey(i)) {
+        		hm.put(i, hm.get(i)+1);
+        	}else {
+        		hm.put(i, 1);
+        	}
         }
+
          
         // find max frequency.
-        int max_count = 0, res = -1;
+        int max_count = 0, result = -1;
          
-        for(Entry<Integer, Integer> val : hm.entrySet())
+        for(Map.Entry<Integer, Integer> entry : hm.entrySet())
         {
-            if (max_count < val.getValue())
+            if (entry.getValue() > max_count)
             {
-                res = val.getKey();
-                max_count = val.getValue();
+                max_count = entry.getValue();
+                result = entry.getKey();
             }
         }
          
-        return res;
+        return result;
     }
      
     // Driver code
     public static void main (String[] args) {
          
         int arr[] = {40,50,30,40,50,30,30,5,5,5,5,5};
-        int n = arr.length;
-         
-        System.out.println(mostFrequent(arr, n));
+        
+        int r = mostFrequent(arr);
+        
+        System.out.println(r);
     }
 
 }
